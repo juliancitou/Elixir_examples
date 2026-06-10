6b882342b207badf61da230ebb5a151443f35f38
juliancitou <22690208@tecvalles.mx>
2026-06-10 16:56:32 -0600

    Aun sin resolverse nada xd

--- case_example.exs
+++ case_example.exs
@@ -1,34 +1,36 @@
+# Quisiera tener una tupla inicial la cual se llama tuplaDefecto la cual quiero que cambie su estado por uno diferente, esta tupla se
+# compone de un atomo en la posicion 0 un string en la posicion 1 y quisiera que cambiara si en dado caso la opcion de la tupla
 # tupla inicial
-exp1 = {:hola, "mensaje por defecto"}
+tuplaDefecto = {:hola, "mensaje por defecto"}
 
 # Cambiar tupla
-IO.puts("\nEscribe la opcion a guardar en la tupla\nOpcion 1) :ok\nOpcion 2) :error")
-tuplaPosicion0 = String.trim(IO.gets("Escribe tu opcion (1 / 2): "))
-tuplaPosicion1 = String.trim(IO.gets("Ahora escriba el mensaje o numero: "))
+IO.puts("\nEscribe la opcion a guardar en la tupla\nOpcion 1) :hola_que_tal\nOpcion 2) :adios")
+posicion0 = String.trim(IO.gets("Escribe tu opcion (1 / 2): "))
+IO.inspect(posicion0)
+posicion1 = String.trim(IO.gets("Ahora escriba el mensaje o numero: "))
 
-if tuplaPosicion0 = "1" do
-  {:hola_que_tal, ^tuplaPosicion1} = exp1
-  IO.inspect(exp1)
+case posicion0 do
+  posicion0 = "1" ->
+    tuplaDefecto = {:hola_que_tal, posicion1}
 
-  if tuplaPosicion0 = "2" do
-    {:adios, ^tuplaPosicion1} = exp1
-    IO.inspect(exp1)
-  else
-    "No se encontro ningun resutlado"
-  end
+  posicion0 = "2" ->
+    tuplaDefecto = {:adios, posicion1}
+
+  true ->
+    "\nEste es un mensaje del case"
 end
 
-if exp1 = true do
+if tuplaDefecto = true do
   output =
-    case exp1 do
-      {:hola_que_tal, ^tuplaPosicion1} when is_number(^tuplaPosicion1) ->
-        "Posicion 0 = :ok\nPosicion1 = escribistes el numero #{^tuplaPosicion1}"
+    case tuplaDefecto do
+      {:hola_que_tal, posicion1} when is_number(posicion1) ->
+        "Posicion 0 = :ok\nPosicion1 = escribistes el numero #{posicion1}"
 
-      {:hola_que_tal, tuplaPosicion1} ->
-        "Posicion0 = :ok\nPosicion1 = Escribistes un mensaje el cual es #{tuplaPosicion1}"
+      {:hola_que_tal, posicion1} ->
+        "Posicion0 = :ok\nPosicion1 = Escribistes un mensaje el cual es #{posicion1}"
 
-      {:adios, tuplaPosicion1} ->
-        "Resultado cuando tiene :error  #{tuplaPosicion1}"
+      {:adios, posicion1} ->
+        "Resultado cuando tiene :error  #{posicion1}"
 
       _ ->
         "Matcheo cualquier cosa"
